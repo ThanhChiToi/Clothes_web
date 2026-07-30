8d95b2e160613cd8cee531f913ce11b6ab7ba5bb
Thien <tranthanhthien1852000@gmail.com>
2026-07-30 16:49:17 +0700

    update card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,15 @@ export const routes: Routes = [
       {
         path: '', // Đường dẫn thực tế: /leave/create
         loadComponent: () => import('./modules/home/home-page/home-page.component').then(c => c.HomePageComponent)
-      }
+      },
+      {
+        path: 'products',
+        loadComponent: () => import('./modules/product/product/product.component').then(c => c.ProductComponent)
+      },
+      {
+        path: 'product/:id',
+        loadComponent: () => import('./modules/product/product-detail/product-detail.component').then(c => c.ProductDetailComponent)
+      },
     ]
   },
   { path: '**', redirectTo: '' }
